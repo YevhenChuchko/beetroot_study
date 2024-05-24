8a454ae57d6fee267e3563df0b4e842dcec65f51
YevhenChuchko <gcworkua@gmail.com>
2024-05-24 10:48:49 +0300

    add find node by id

--- HW_Data structures (lists)/linked_list.py
+++ HW_Data structures (lists)/linked_list.py
@@ -7,6 +7,7 @@ class Node:
     def __str__(self):
         return f"Node {self.id}: value {self.value}"
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -33,19 +34,43 @@ class LinkedList:
 
     def add_node(self, value):
         _id = len(self) + 1
+        new_node = Node(_id, value)
         if not self.head:
-            self.head = Node(_id, value)
+            self.head = new_node
         else:
             current = self.head
             while current.next:
                 current = current.next
-            current.next = Node(_id, value)
+            current.next = new_node
+
+    def find_node_by_id(self, _id):
+        current = self.head
+        while current:
+            if current.id == _id:
+                return current
+            current = current.next
+        return None
+
+    def remove_last_node(self):
+        if not self.head:
+            return None
+        if not self.head.next:
+            removed_node = self.head
+            self.head = None
+            return removed_node
+        current = self.head
+        while current.next and current.next.next:
+            current = current.next
+        removed_node = current.next
+        current.next = None
+        return removed_node
 
     def print_nodes(self):
         current = self.head
         while current:
             print(current)
             current = current.next
+
 
 if __name__ == "__main__":
     container = LinkedList()
@@ -55,5 +80,21 @@ if __name__ == "__main__":
     print(len(container))
     container.print_nodes()
 
-    for node in container:
+
+    print("\nПошук вузла за id:")
+    node = container.find_node_by_id(2)
+    if node:
         print(node)
+    else:
+        print("Node not found")
+
+
+    print("\nВидалення останнього вузла:")
+    removed_node = container.remove_last_node()
+    if removed_node:
+        print(f"Removed {removed_node}")
+    else:
+        print("Nothing to remove")
+
+    print("\nСписок після видалення останнього вузла:")
+    container.print_nodes()
